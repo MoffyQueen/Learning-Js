db4b334d9dcb718e6082802b44d7e13efdc8aadf
MoffyQueen <oretimofeoladipupo@gmail.com>
2023-06-01 15:26:40 +0100

    control flow

--- app.js
+++ app.js
@@ -88,17 +88,17 @@
 // console.log(email2.includes("mofe"));
 // console.log(email.indexOf("a"));
 // console.log(email2.lastIndexOf("a"));
-const user = "     Mubarak     ";
-const password = "   123456789   ";
-console.log(user.length, password.length);
+// const user = "     Mubarak     ";
+// const password = "   123456789   ";
+// console.log(user.length, password.length);
 
-const formattedUser = user.trim();
-const formattedPassword = password.trimStart();
-console.log(formattedUser.length);
-console.log(formattedPassword.length);
+// const formattedUser = user.trim();
+// const formattedPassword = password.trimStart();
+// console.log(formattedUser.length);
+// console.log(formattedPassword.length);
 
-console.log(email2.startsWith("m"));
-console.log(email2.endWith(".com"));
+// console.log(email2.startsWith("m"));
+// console.log(email2.endWith(".com"));
 
 // // template literals
 // const author = "Chinua Achebe";
@@ -111,98 +111,208 @@ console.log(email2.endWith(".com"));
 // console.log(bookDescription)
 // Numbers - 5 6.3 45000
 // math operators - + - * / ** %
-console.log(6 % 2); 
-console.log(5 % 2);
+// console.log(6 % 2);
+// console.log(5 % 2);
 
-let total = 50;
-// increase the value of total by 10
-total = total + 10;
-total += 10;
-total += 5;
+// let total = 50;
+// // increase the value of total by 10
+// total = total + 10;
+// total += 10;
+// total += 5;
 
-// decrease the val of total by 12
-total = total - 12;
-total -= 12;
-total -= 10;
+// // decrease the val of total by 12
+// total = total - 12;
+// total -= 12;
+// total -= 10;
 
-console.log(total)
+// console.log(total)
 
-let post = 40;
-post += 15;
-post -= 15;
-post *= 2;
-post /= 5;
+// let post = 40;
+// post += 15;
+// post -= 15;
+// post *= 2;
+// post /= 5;
 
-console.log(post);
+// console.log(post);
 
-let likes = 0;
-likes += 1;
-likes += 1;
-likes += 1;
-likes -= 1;
-likes--;
-likes++;
+// let likes = 0;
+// likes += 1;
+// likes += 1;
+// likes += 1;
+// likes -= 1;
+// likes--;
+// likes++;
 
-console.log(likes)
+// console.log(likes)
 
-// NaN - not a number
+// // NaN - not a number
 
-console.log(5 ** "hello");
+// console.log(5 ** "hello");
 
-// Boolean true and false
-console.log(true);
-console.log(false)
-const country = "Nigeria";
-const continent = "Africa";
-let population = "1000000";
+// // Boolean true and false
+// console.log(true);
+// console.log(false)
+// const country = "Nigeria";
+// const continent = "Africa";
+// let population = "1000000";
 
-console.log(country, continent, population);
-const isIsland = false;
-const language = "English";
+// console.log(country, continent, population);
+// const isIsland = false;
+// const language = "English";
 
-population /= 2;
-console.log(population);
+// population /= 2;
+// console.log(population);
 
-population++;
-console.log(population);
-// template literals -
-// decription - Togo is in Africa and its population of 1000000 people speak english
-const description2 = `${country} is in ${continent} and its population of ${population} people speak ${language}`
-console.log(description2);
-const description =
-country +  "is in" + continent + "and its population of" + population
-console.log(description)
+// population++;
+// console.log(population);
+// // template literals -
+// // decription - Togo is in Africa and its population of 1000000 people speak english
+// const description2 = `${country} is in ${continent} and its population of ${population} people speak ${language}`
+// console.log(description2);
+// const description =
+// country +  "is in" + continent + "and its population of" + population
+// console.log(description)
 
-// Logical Operators and && or || not !
+// // Logical Operators and && or || not !
 
-console.log(10 == "10");
-console.log(10 === "10");
-console.log("a" > "A"); //
+// console.log(10 == "10");
+// console.log(10 === "10");
+// console.log("a" > "A"); //
 
-// type conversion and type coercion
+// // type conversion and type coercion
 
-// string numbers
-// strings to numbers
+// // string numbers
+// // strings to numbers
 
-const str = "90";
-console.log(typeof str);
-const converted = Number(str);
-console.log(typeof converted);
+// const str = "90";
+// console.log(typeof str);
+// const converted = Number(str);
+// console.log(typeof converted);
 
-// numbers to string
+// // numbers to string
 
-const num = 900000;
-console.log(typeof num);
-const converted2 = String(num);
-console.log(typeof converted2);
+// const num = 900000;
+// console.log(typeof num);
+// const converted2 = String(num);
+// console.log(typeof converted2);
 
-// type coercion
+// // type coercion
 
-const num1 = 8;
-const num2 = "7";
+// const num1 = 8;
+// const num2 = "7";
 
-console.log(num1 + num2);
-console.log(typeof res);
+// console.log(num1 + num2);
+// console.log(typeof res);
 
-// Null - null
-// Undefined - undefined
+// // Null - null
+// let age = null;
+// console.log(age, age + 4, 'my age is ${age}');
+// // Undefined - undefined
+// let year;
+// console.log(year);
+// console.log(year, year + 4, 'the year is ${year}');
+// //truthy -
+// console.log(Boolean(""));
+// and falsy values 0, null, undefined
+
+//control flow
+//conditional statement
+//if (condition){code}
+// const password = oretimofe;
+// if (password.length > 6) {
+// console.log("Your password is strong");
+// }
+// if (false) {
+// console.log("working");
+// }
+// if else
+// if(condition){
+// CODE 1
+//} else
+// if (password.length > 6 && password.includes("@")) {
+// console.log("Your password is strong");
+// } else {
+// console.log("Your password is weak");
+//     }
+    const country = "Hungary";
+    const continent = "Europe";
+    const population = 12;
+    const isIsland = false;
+    const language = "French";
+
+    if (language === "English" && population < 50 && isIsland) {
+        console.log('You should live in ${country}');
+    } else {
+        console.log('${country} does not meet your criteria');
+    }
+// const age = 18;
+// if (age >= 18) {
+//     console.log("You are an adult");
+// } else {
+//     console.log("You are not one");
+//     }
+// if (condition) else if (another condition) else if (condition){}else{}
+
+const password = "mofe";
+// recommended > 7 an dinclude @, strong > 7 and weak
+
+if (password.length >= 7 && password.includes("@")) {
+    console.log("Your password is recommended");
+} else if (password.length >= 7) {
+    console.log("Your password is strong");
+} else {
+    console.log("Your password is weak")
+}
+// whether a number is positive, negative or zero
+const num = "3";
+
+if (num > 0) {
+    console.log('${num} is a positive');
+} else if (num < 0) {
+    console.log('${num} is a negative number');
+} else {
+    console.log("This is Zero");
+}
+// even or odd
+
+if (num % 2 === 0) {
+    console.log("This is EVEN");
+} else {
+    console.log("This is ODD");
+}
+const age = 18
+if (age < 18) {
+    console.log("Welcome to the baby section");
+} else if (age <= 50) {
+    console.log("Welcome to the club");
+} else  {
+    console.log("Welcome to VIP SECTION");
+}
+// SWITCH STATeMENT
+const day = 1
+switch (day) {
+    case 1:
+        console.log("MONDAY");
+        break;
+    case 2:
+        console.log("TUESDAY");
+        break;
+    case 3:
+        console.log("DAY");
+        break;
+    case 4:
+        console.log("DAY");
+        break;
+    case 5:
+        console.log("DAY");
+        break;
+    case 6:
+        console.log("DAY");
+        break;
+    case 1:
+        console.log("DAY");
+        break;
+    
+    default:
+        break;
+}
